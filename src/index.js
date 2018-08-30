dddb47c274469960204e4a74cc6e248955b794e5
mayconbalves <mayconmustaine@hotmail.com>
2018-08-30 15:45:49 -0300

    refactor(*): change directories and files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './scripts/containers/App';
+import registerServiceWorker from './scripts/core/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
