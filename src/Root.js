cdfd76880071fc8cd6794a7fc2a917fa56ea4310
mayconbalves <mayconmustaine@hotmail.com>
2018-08-31 07:45:04 -0300

    feat(todo): create container and create router for container

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './scripts/containers/App'
+import Todo from './scripts/containers/Todo'
 
 const Root = () => (
   <Router>
     <div>
       <Route exact path='/' component={App} />
+      <Route path='/todo-list' component={Todo} />
     </div>
   </Router>
 )
--- src/scripts/containers/App.js
+++ src/scripts/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render () {
@@ -7,6 +8,11 @@ class App extends Component {
         <header className='App-header'>
           <h1 className='App-title'>Todo List</h1>
         </header>
+        <nav>
+          <ul>
+            <li><Link to='/todo-list'>Todo-List</Link></li>
+          </ul>
+        </nav>
       </div>
     )
   }
