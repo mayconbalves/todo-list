f0bd569e886d9e0d452576e4e7accc43b426ca64
mayconbalves <mayconmustaine@hotmail.com>
2018-08-31 08:13:52 -0300

    feat(*): create component menu

---
+++ src/scripts/components/utils/Menu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Menu extends Component {
+  render () {
+    return (
+      <main>
+        <nav>
+          <ul className='list'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/todo-list'>Todo-List</Link></li>
+          </ul>
+        </nav>
+
+      </main>
+    )
+  }
+}
+
+export default Menu
