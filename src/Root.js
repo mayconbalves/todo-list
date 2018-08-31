254d6bfb12bd2ea629ffb53f652fc1029187752a
mayconbalves <mayconmustaine@hotmail.com>
2018-08-31 16:06:14 -0300

    chore(root): add component switch

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Dashboard from './scripts/containers/Dashboard'
 import Todo from './scripts/containers/Todo'
 
 const Root = () => (
   <Router>
-    <div>
+    <Switch>
       <Route exact path='/' component={Dashboard} />
       <Route path='/todo-list' component={Todo} />
-      <Redirect from='*' to='/' />
-    </div>
+      <Redirect to='/' />
+    </Switch>
   </Router>
 )
 
