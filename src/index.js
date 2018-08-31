437d38e041c83db60c45c0613910b4145acc9476
mayconbalves <mayconmustaine@hotmail.com>
2018-08-31 08:53:23 -0300

    feat(*): add bootstrap dependencie

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import Root from './Root'
 import registerServiceWorker from './scripts/core/registerServiceWorker'
 import './styles/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
