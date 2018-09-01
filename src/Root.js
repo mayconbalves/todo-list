a15d51e2d414378f11925766efc902d4e1a305de
mayconbalves <mayconmustaine@hotmail.com>
2018-09-01 13:45:05 -0300

    feat(*): add Provider for control store application

--- src/Root.js
+++ src/Root.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import Dashboard from './scripts/containers/Dashboard'
 import Todo from './scripts/containers/Todo'
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={Dashboard} />
-      <Route path='/todo-list' component={Todo} />
-      <Redirect to='/' />
-    </Switch>
-  </Router>
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route path='/todo-list' component={Todo} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  </Provider>
 )
+
+Root.propTypes = {
+  store: PropTypes.object
+}
 
 export default Root
