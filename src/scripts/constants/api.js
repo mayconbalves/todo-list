b1be008ae30a7b7d06d5aab00def92f9eb9dfe7c
mayconbalves <mayconmustaine@hotmail.com>
2018-09-01 15:41:51 -0300

    feat(*): create component api

---
+++ src/scripts/constants/api.js
@@ -0,0 +1,7 @@
+const apiKeyMlab = '6b7477mEkxRuqQhGgZwo0i2tbHnerl7a'
+const url = 'https://api.mlab.com/api/1/databases/maycon-todo-list/collections/todo-list'
+
+export default {
+  URL: url,
+  TODO_LIST: `${url}?apiKey=${apiKeyMlab}`
+}
