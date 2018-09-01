32984551b6c1da3b5b7ba8fbd65e5a059f249adb
mayconbalves <mayconmustaine@hotmail.com>
2018-09-01 13:42:32 -0300

    feat(todo): create reducer for todo

---
+++ src/scripts/reducers/todo.js
@@ -0,0 +1,35 @@
+import {
+  ADD_TODO_SUCCESS,
+  TODO_LIST_SUCCESS,
+  TODO_LIST_ERROR
+} from '../constants/actionTypes'
+
+const initialState = {
+  description: '',
+  list: [],
+  error: null
+}
+
+export default function todo (state = initialState, action) {
+  switch (action.type) {
+    case TODO_LIST_SUCCESS:
+      return {
+        ...state,
+        list: action.payload.data,
+        error: null
+      }
+    case TODO_LIST_ERROR:
+      return {
+        list: [],
+        error: action.payload
+      }
+    case ADD_TODO_SUCCESS:
+      return {
+        ...state,
+        description: '',
+        error: null
+      }
+    default:
+      return state
+  }
+}
