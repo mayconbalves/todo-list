38b7b43af804b99fe177f2c6ce9b18d79c0cf264
mayconbalves <mayconmustaine@hotmail.com>
2018-09-01 15:43:18 -0300

    refactor(todo): remove const apikeyMlab and url

--- src/scripts/actions/todo.js
+++ src/scripts/actions/todo.js
@@ -5,11 +5,10 @@ import {
 
 import axios from 'axios'
 
-const apiKeyMlab = '6b7477mEkxRuqQhGgZwo0i2tbHnerl7a'
-const URL = `https://api.mlab.com/api/1/databases/maycon-todo-list/collections/todo-list?apiKey=${apiKeyMlab}`
+import API_URL from '../constants/api'
 
 export const fetchTodoList = () => {
-  const request = axios.get(URL)
+  const request = axios.get(API_URL.TODO_LIST)
   return {
     type: TODO_LIST_SUCCESS,
     payload: request
@@ -17,7 +16,7 @@ export const fetchTodoList = () => {
 }
 
 export const fetchAddTodo = description => {
-  const request = axios.post(URL, { description, done: false })
+  const request = axios.post(API_URL.TODO_LIST, { description, done: false })
   return {
     type: ADD_TODO_SUCCESS,
     payload: request
