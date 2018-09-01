aa56ff9527461cad703aa4f65c8e44fabd13fe0f
mayconbalves <mayconmustaine@hotmail.com>
2018-09-01 13:45:52 -0300

    feat(*) add middleares inside application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
 import Root from './Root'
+import reducers from './scripts/reducers/index'
 import registerServiceWorker from './scripts/core/registerServiceWorker'
 import './styles/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+const devTools = window._REDUX_DEVTOOLS_EXTENSION_ && window.window._REDUX_DEVTOOLS_EXTENSION_()
+
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 registerServiceWorker()
