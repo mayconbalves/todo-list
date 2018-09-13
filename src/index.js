530ba9c6e1c75e8ad465899037279920505f622d
mayconbalves <mayconmustaine@hotmail.com>
2018-09-13 19:46:18 -0300

    refactor(index): remove code of store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
 
 import Root from './Root'
-import reducers from './scripts/reducers/index'
 import registerServiceWorker from './scripts/core/registerServiceWorker'
+import store from './scripts/store/store.dev'
 import './styles/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 registerServiceWorker()
