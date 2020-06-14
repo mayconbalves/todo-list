2113b1b9fcae63b9f09a1251a11fa3d50d59ddf0
mayconbalves <mayconmustaine@hotmail.com>
2020-06-14 12:14:17 -0300

    feat(backend): create entry point for app

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const product = require('./routes/todo.router') // Imports routes for the products
+
+const mongoDB = process.env.MONGODB_URI
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser : true
+})
+
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+// baseUrl:  http://localhost:8080/products/
+app.use('/products', product)
+
+const port = 8080
+
+app.listen(port, () => {
+  console.log('Server start on port ' + port)
+})
