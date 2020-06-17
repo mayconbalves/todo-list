4561e158e5a2d6c14dc0d003ff09fd603cd9f8bc
mayconbalves <mayconmustaine@hotmail.com>
2020-06-16 22:47:43 -0300

    chore(backend): comment unused routes

--- backend/src/routes/todo.router.js
+++ backend/src/routes/todo.router.js
@@ -7,9 +7,9 @@ const todo_controller = require('../controllers/todo.controller')
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/list', todo_controller.todo_list)
-router.get('/:id', todo_controller.product_details)
+// router.get('/:id', todo_controller.product_details)
 router.post('/create', todo_controller.todo_create)
-router.put('/:id/update', todo_controller.product_update)
-router.delete('/:id/delete', todo_controller.product_delete)
+// router.put('/:id/update', todo_controller.product_update)
+// router.delete('/:id/delete', todo_controller.product_delete)
 
 module.exports = router
