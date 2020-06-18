e50862014a7defd6114c3e78e2e36b1730baddcc
mayconbalves <mayconmustaine@hotmail.com>
2020-06-17 21:18:32 -0300

    refactor(frontend): remove unnescessary code

--- frontend/src/scripts/constants/api.js
+++ frontend/src/scripts/constants/api.js
@@ -1,7 +1,5 @@
-export const apiKeyMlab = '6b7477mEkxRuqQhGgZwo0i2tbHnerl7a'
-const url = 'https://api.mlab.com/api/1/databases/maycon-todo-list/collections/todo-list'
+const baseUrl = 'http://localhost:8080/todos'
 
 export default {
-  URL: url,
-  TODO_LIST: `${url}?apiKey=${apiKeyMlab}`
+  URL: baseUrl
 }
