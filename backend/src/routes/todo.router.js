96c27fb55776c9f9719b95db6933b71e218318e0
mayconbalves <mayconmustaine@hotmail.com>
2020-06-22 20:42:38 -0300

    feat(backend): create update router

--- backend/src/routes/todo.router.js
+++ backend/src/routes/todo.router.js
@@ -9,7 +9,7 @@ const todo_controller = require('../controllers/todo.controller')
 router.get('/list', todo_controller.todo_list)
 // router.get('/:id', todo_controller.product_details)
 router.post('/create', todo_controller.todo_create)
-// router.put('/:id/update', todo_controller.product_update)
+router.put('/:id/update', todo_controller.todo_update)
 router.delete('/:id/delete', todo_controller.todo_delete)
 
 module.exports = router
