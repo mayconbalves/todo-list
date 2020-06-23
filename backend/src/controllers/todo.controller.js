c8952f29e6883768f32d0ee664b8edc3395790db
mayconbalves <mayconmustaine@hotmail.com>
2020-06-23 20:42:53 -0300

    fix(backend): change names of methods

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -2,28 +2,28 @@ const Todo = require('../models/todo.model')
 
 module.exports = {
   async todo_list(req, res) {
-    const product = await Product.find({}).sort('-createdAt-')
-    return res.json(product)
+    const todo = await Todo.find({}).sort('-createdAt-')
+    return res.json(todo)
   },
 
   async todo_create(req, res) {
-    const product = await Product.create(
+    const todo = await Todo.create(
         {
           description: req.body.description,
           done: req.body.done
         }
     )
-    return res.json(product)
+    return res.json(todo)
   },
 
   async todo_delete(req, res) {
-    const product = await Product.findByIdAndRemove(req.params.id)
-    return res.json(product)
+    const todo = await Todo.findByIdAndRemove(req.params.id)
+    return res.json(todo)
   },
 
   async todo_update(req, res) {
-    const product = await Product.findByIdAndUpdate(req.params.id, {$set: req.body})
+    const todo = await Todo.findByIdAndUpdate(req.params.id, {$set: req.body})
 
-    return res.json(product)
+    return res.json(todo)
   }
 }
