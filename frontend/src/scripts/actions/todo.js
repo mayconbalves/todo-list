6865b5072483e8d871a22734c97a5011c3737a2a
mayconbalves <mayconmustaine@hotmail.com>
2020-06-22 21:45:07 -0300

    fix(frontend): eslint errors

--- frontend/src/scripts/actions/todo.js
+++ frontend/src/scripts/actions/todo.js
@@ -1,101 +1,103 @@
-import {
-  ADD_TODO_SUCCESS,
-  ADD_TODO_ERROR,
-  DELETE_TODO_ERROR,
-  DELETE_TODO_SUCCESS,
-  TODO_LIST_SUCCESS,
-  TODO_LIST_ERROR,
-  DONE_TODO_SUCCESS,
-  DONE_TODO_ERROR,
-  PENDING_TODO_SUCCESS,
-  PENDING_TODO_ERROR
-} from '../constants/actionTypes'
-
 import axios from 'axios'
+import {
+	ADD_TODO_SUCCESS,
+	ADD_TODO_ERROR,
+	DELETE_TODO_ERROR,
+	DELETE_TODO_SUCCESS,
+	TODO_LIST_SUCCESS,
+	TODO_LIST_ERROR,
+	DONE_TODO_SUCCESS,
+	DONE_TODO_ERROR,
+	PENDING_TODO_SUCCESS,
+	PENDING_TODO_ERROR
+} from '../constants/actionTypes'
 import API from '../constants/api'
 
-export const fetchTodoList = () => dispatch => {
-  axios.get(`${API.URL}/list`)
-    .then(response => dispatch(todoListSuccess(response.data, dispatch)))
-    .catch(error => dispatch(todoListError(error)))
+const todoListSuccess = (data) => ({
+	type: TODO_LIST_SUCCESS,
+	payload: data
+})
+
+const todoListError = (error) => ({
+	type: TODO_LIST_ERROR,
+	payload: error
+})
+export const fetchTodoList = () => (dispatch) => {
+	axios
+		.get(`${API.URL}/list`)
+		.then((response) => dispatch(todoListSuccess(response.data, dispatch)))
+		.catch((error) => dispatch(todoListError(error)))
 }
 
-const todoListSuccess = data => ({
-  type: TODO_LIST_SUCCESS,
-  payload: data
+const addTodoSuccess = (data) => ({
+	type: ADD_TODO_SUCCESS,
+	payload: data
 })
 
-const todoListError = error => ({
-    type: TODO_LIST_ERROR,
-    payload: error
+const addTodoError = (error) => ({
+	type: ADD_TODO_ERROR,
+	payload: error
 })
 
-export const fetchAddTodo = description => dispatch => {
-  axios.post(`${API.URL}/create`, { description, done: false })
-    .then(respomse => dispatch(addTodoSuccess(respomse.data, dispatch)))
-    .catch(error => dispatch(addTodoError(error)))
-    .then(() => dispatch(fetchTodoList()))
+export const fetchAddTodo = (description) => (dispatch) => {
+	axios
+		.post(`${API.URL}/create`, { description, done: false })
+		.then((respomse) => dispatch(addTodoSuccess(respomse.data, dispatch)))
+		.catch((error) => dispatch(addTodoError(error)))
+		.then(() => dispatch(fetchTodoList()))
 }
 
-const addTodoSuccess = data => ({
-  type: ADD_TODO_SUCCESS,
-  payload: data
+const deleteTodoSuccess = (data) => ({
+	type: DELETE_TODO_SUCCESS,
+	payload: data
 })
 
-const addTodoError = error => ({
-  type: ADD_TODO_ERROR,
-  payload: error
+const deleteTodoError = (error) => ({
+	type: DELETE_TODO_ERROR,
+	payload: error
 })
 
-export const fetchDeleteTodo = _id => dispatch => {
-  axios.delete(`${API.URL}/${_id}/delete`)
-    .then(response => dispatch(deleteTodoSuccess(response.data)))
-    .catch(error => dispatch(deleteTodoError(error)))
-    .then(() => dispatch(fetchTodoList()))
+export const fetchDeleteTodo = (_id) => (dispatch) => {
+	axios
+		.delete(`${API.URL}/${_id}/delete`)
+		.then((response) => dispatch(deleteTodoSuccess(response.data)))
+		.catch((error) => dispatch(deleteTodoError(error)))
+		.then(() => dispatch(fetchTodoList()))
 }
 
-const deleteTodoSuccess = data => ({
-  type: DELETE_TODO_SUCCESS,
-  payload: data
+const doneTodoSuccess = (data) => ({
+	type: DONE_TODO_SUCCESS,
+	payload: data
 })
 
-const deleteTodoError = error => ({
-  type: DELETE_TODO_ERROR,
-  payload: error
+const doneTodoError = (error) => ({
+	type: DONE_TODO_ERROR,
+	payload: error
 })
 
-export const fetchDoneTodo = list => dispatch => {
-  const _id = list._id
-  axios.put(`${API.URL}/${_id}/update`, { done: true })
-    .then(response => dispatch(doneTodoSuccess(response.data)))
-    .catch(error => dispatch(doneTodoError(error)))
-    .then(() => dispatch(fetchTodoList()))
+export const fetchDoneTodo = (list) => (dispatch) => {
+	const { _id } = list
+	axios
+		.put(`${API.URL}/${_id}/update`, { done: true })
+		.then((response) => dispatch(doneTodoSuccess(response.data)))
+		.catch((error) => dispatch(doneTodoError(error)))
+		.then(() => dispatch(fetchTodoList()))
 }
 
-const doneTodoSuccess = data => ({
-  type: DONE_TODO_SUCCESS,
-  payload: data
+const pendingTodoSuccess = (data) => ({
+	type: PENDING_TODO_SUCCESS,
+	payload: data
 })
 
-const doneTodoError = error => ({
-  type: DONE_TODO_ERROR,
-  payload: error
+const pendingTodoError = (error) => ({
+	type: PENDING_TODO_ERROR,
+	payload: error
 })
-
-export const fetchPendingTodo = list => dispatch => {
-  const _id = list._id
-  axios.put(`${API.URL}/${_id}/update`, { done: false })
-    .then(response => dispatch(pendingTodoSuccess(response.data)))
-    .catch(error => dispatch(pendingTodoError(error)))
-    .then(() => dispatch(fetchTodoList()))
+export const fetchPendingTodo = (list) => (dispatch) => {
+	const { _id } = list
+	axios
+		.put(`${API.URL}/${_id}/update`, { done: false })
+		.then((response) => dispatch(pendingTodoSuccess(response.data)))
+		.catch((error) => dispatch(pendingTodoError(error)))
+		.then(() => dispatch(fetchTodoList()))
 }
-
-const pendingTodoSuccess = data => ({
-  type: PENDING_TODO_SUCCESS,
-  payload: data
-})
-
-const pendingTodoError = error => ({
-  type: PENDING_TODO_ERROR,
-  payload: error
-})
